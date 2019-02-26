cad8987a9cb978d34bf0c9811e89f6e768987586
Mitchel Parish <mitchelparish@gmail.com>
2019-02-26 20:35:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,7 +18,7 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end
 
-  get '/multiply/num1:/num2:' do
+  get '/multiply/:num1/:num2' do
     @x = params[:num1].to_i
     @y = params[:num2].to_i
     "#{@x * @y}"
